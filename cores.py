7e3821a488f8007889015be3045590aaceba86a2
LLxD <limalucas454@gmail.com>
2019-05-15 19:24:58 -0300

    Adicionada a lista de cores já vistas e ideias para a implementação do resto do código

--- cores.py
+++ cores.py
@@ -3,9 +3,24 @@ from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import ColorSensor
 # declaração do sensor
 sensordecor=ColorSensor(INPUT_1)
+#declaração de uma lista vazia para receber as cores já lidas
+cores = []
 while True:
     #entrada no loop de leitura
-    print(sensordecor.color_name)
+    cor = sensordecor.color_name
+    #adição da cor vista a lista caso ela não seja repetida
+    if cor not in cores:
+        cores.append(cor)
+        #se cor nova,adicionar tomada de decisão para direção por tentativa e erro
+    else :
+        #if cor == 'White':
+        #continuar função caminho
+        print("Cor já vista")
+        #repetir a decisão descoberta na primeira etapa
+    print('A cor atual é ',cor)
+    #printar a lista de cores obtida
+    for i in len(cores):
+        print(cores[i])
     #parada
     a = str(input("De novo? "))
     if (a == 'N'):
